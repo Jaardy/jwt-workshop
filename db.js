baaab2c111ea364d3ba8240bb3ecb8ae71197c87
Jordan <43437534+Jaardy@users.noreply.github.com>
2022-11-29 15:59:16 +0000

    token middleware

---
+++ db.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const path = require("path");
+const { Sequelize, Model, DataTypes, ValidationError } = require("sequelize");
+
+exports.sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+  logging: false
+});
+
+exports.User = class User extends Model {};
+
+exports.User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: exports.sequelize,
+  }
+);
+
+exports.Message = class Message extends Model {};
+
+exports.Message.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: exports.sequelize,
+  }
+);
+
+exports.ValidationError = ValidationError;
